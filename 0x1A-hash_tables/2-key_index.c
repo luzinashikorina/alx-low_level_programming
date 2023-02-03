9fdf4ee65be48b17aa9a269277a507e6decfe63c
luzinashikorina <luzabiniam01@gmail.com>
2023-02-03 20:47:42 +0300

    Success All

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -3,7 +3,8 @@
  * key_index - gives me the index of a key
  * @key: the key
  * @size: size of the array of the hash table
- * Return: the index at which the key/value pair should be stored in array pf hash table
+ * Return: the index at which the key/value pair
+ * should be stored in array pf hash table
  */
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
