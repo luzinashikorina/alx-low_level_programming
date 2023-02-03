118e7080785f3be501f88c254bd5c04cbebacd27
luzinashikorina <luzabiniam01@gmail.com>
2023-02-03 20:17:21 +0300

    Success All

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,38 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "hash_tables.h"
+
+/**
+ * hash_table_create - creates a hash table
+ * @size: the size of the array
+ * Return: a pointer to the newly created hash table
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *table;
+	hash_node_t **node;
+	unsigned long int i = 0;
+
+	table = malloc(sizeof(hash_table_t));
+	if (table == NULL)
+		return (NULL);
+	if ( size == 0)
+		return (NULL);
+	table->size = size;
+
+	node = malloc(sizeof(hash_node_t) * size);
+	if (node == NULL)
+	{
+		free(table);
+		table = NULL;
+		return (NULL);
+	}
+	table->array = node;
+	for (i = 0; i < size; i++)
+		table->array[i] = NULL;
+
+	return (table);
+
+}
+
---
+++ 0x1A-hash_tables/2-key_index.c
@@ -0,0 +1,15 @@
+#include "hash_tables.h"
+/**
+ * key_index - gives me the index of a key
+ * @key: the key
+ * @size: size of the array of the hash table
+ * Return: the index at which the key/value pair should be stored in array pf hash table
+ */
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+	unsigned long int hash_value;
+
+	hash_value = hash_djb2(key);
+	return (hash_value % size);
+
+}
